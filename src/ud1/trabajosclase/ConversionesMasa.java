7b25e004e9cb8124326b87cbb3d6859909f2590d
HugoChSa <hchansaba@ieschandomonte.edu.com>
2024-10-15 13:10:18 +0200

    conversiones masa estaba hecho en otra carpeta, lo pase a esta

--- src/ud1/trabajosclase/ConversionesMasa.java
+++ src/ud1/trabajosclase/ConversionesMasa.java
@@ -1,7 +1,41 @@
 package ud1.trabajosclase;
+
+        /**EP0123_ConversionMasa. Escribe un programa que convierta unidades de masa entre kilogramos y libras o viceversa. El programa solicitará al usuario la cantidad de masa y la unidad
+ * de medida origen (kilogramos o libras). 
+ * 1 kilogramo = 1000 gramos 
+ * 1 libra = 453.592 gramos
+ * El programa deberá manejar las conversiones automáticamente y mostrar el resultado al usuario. Utiliza el operador ternario para evaluar la unidad de medida introducida por el
+ * usuario. El programa mostrará por pantalla el resultado con la unidad de medida adecuada.
+ */
+import java.util.Scanner;
 public class ConversionesMasa {
     public static void main(String[] args) {
-        System.out.println("Dime una unidad en kilogramos: ");
+        double masaOrigen, masaDestino;
+        
+        final double GRAMOS_LIBRA = 453.592;
+        final double KILOS_LIBRAS = 1000 / GRAMOS_LIBRA;
+        final double LIBRAS_KILOS = GRAMOS_LIBRA / 1000;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce la masa de origen: ");
+        masaOrigen = sc.nextDouble();
+        
+        /*byte unidadDeMedida;
+        System.out.println("Introduce la medida de origen:  (1 = Kilos, 2 = Libras): ");
+        unidadDeMedida = sc.nextByte();*/
+    
+        char unidadDeMedida;
+        System.out.println("Introduce la medida de origen:  (K = Kilos, L = Libras): ");
+        unidadDeMedida = sc.nextLine().charAt(0);
+
+        masaDestino = unidadDeMedida == 'K' ? masaOrigen * KILOS_LIBRAS : masaOrigen * LIBRAS_KILOS;
+
+        String unidadmedidaDestino = unidadDeMedida == 'K' ? "libras" : "kilos";
+
+        sc.close();
+        
+        System.out.printf("La masa en la medida del destino es %.3f %s %n", masaDestino, unidadDeMedida ==1 ? "libras" : "kilos");
+
     }
     
 }
