c9bc5320611d6a118cdf7723caa8c8e9c4c5c003
HugoChSa <hchansaba@ieschandomonte.edu.es>
2025-01-22 12:43:36 +0100

    .

---
+++ src/ud4/ejercicios/ArraysEstadisticas.java
@@ -0,0 +1,68 @@
+package ud4.ejercicios;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraysEstadisticas {
+   
+public static void main(String[] args) {
+    double[] i = new double[0];
+    double numeros;
+
+    System.out.print("Introduce los numeros que formaran parte del Array:");
+    Scanner sc = new Scanner(System.in);
+        numeros = sc.nextDouble();
+    while ( numeros!= -1) {
+        i = Arrays.copyOf(i, i.length + 1);
+        i[i.length - 1] = numeros;
+        
+        System.out.print("Introduce  ");
+        numeros = sc.nextDouble();
+    }
+
+// Declaración de variables y constantes
+/*static int suma(int[] numeros) {
+    for suma = 0:
+    Arra
+}
+
+static int minimo(int[] numeros){
+
+}
+
+
+static int maximo(int[] numeros) {
+
+}
+
+static double mediana(int[] numeros){
+
+}
+
+static int moda(int[] numeros) {
+    
+}
+*/
+int[] numeros1 = {2, 4, 6, 7, 4, 7, 9, 2, 5, 6, 7};
+int[] numeros2 = {2, 4, 6, 7, 4, 9, 2, 5, 6, 7};
+// Proceso y salida
+imprimeEstadisticas(numeros1);
+imprimeEstadisticas(numeros2);
+} 
+
+static void imprimeEstadisticas(int[] numeros) {
+System.out.println("ARRAY ORIGINAL: " + Arrays.toString(numeros));
+System.out.println("---------------------------------------------");
+System.out.println("Longitud: " + numeros.length);
+
+//System.out.println("Suma: " + suma(numeros));
+//System.out.println("Máximo: " + maximo(numeros));
+//System.out.println("Mínimo: " + minimo(numeros));
+//System.out.println("Media: " + media(numeros));
+//System.out.println("Mediana: " + mediana(numeros));
+//System.out.println("Moda: " + moda(numeros));
+Arrays.sort(numeros);
+System.out.println("ARRAY ORDENADO: " + Arrays.toString(numeros));
+System.out.println("---------------------------------------------\n\n");
+}
+}
