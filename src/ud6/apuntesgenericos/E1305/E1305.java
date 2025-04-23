da3aea36bd0e30a836a035576b24da083c03c49b
HugoChSa <hchansaba@ieschandomonte.edu.es>
2025-04-23 13:56:58 +0200

    .

---
+++ src/ud6/apuntesgenericos/E1305/E1305.java
@@ -0,0 +1,16 @@
+package ud6.apuntesgenericos.E1305;
+
+import java.util.function.Function;
+
+/*
+ *
+ */
+public class E1305 {
+    public static void main(String[] args) {
+        
+        
+    }
+    //static <T, V> V[] transformar(T[] t, V[] transf, Function<T, V> f)
+
+   
+}
