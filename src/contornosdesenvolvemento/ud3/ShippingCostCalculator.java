7603e7425b340959c1350775d884eab707c4d556
HugoChSa <hchansaba@ieschandomonte.edu.es>
2025-05-07 12:36:08 +0200

    Restaurada MV

---
+++ src/contornosdesenvolvemento/ud3/ShippingCostCalculator.java
@@ -0,0 +1,20 @@
+package contornosdesenvolvemento.ud3;
+
+    public class ShippingCostCalculator {
+        public static double calculateShipping(double amount, boolean expressDelivery) {
+            if (amount < 0)
+                throw new IllegalArgumentException("O importe non pode ser negativo");
+
+            if (amount > 100)
+                return 0.0;
+
+            double shippingCost = expressDelivery ? 10.0 : 5.0;
+
+            if (amount < 20) {
+                shippingCost += 2.0;
+            }
+
+            return shippingCost;
+        }
+    }
+
